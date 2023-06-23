e3e1833139f0e6f4c8f45b6b48e45b24c558ccb9
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-23 22:13:28 +0200

    feat: exit function

--- src/modules/commandListener.js
+++ src/modules/commandListener.js
@@ -1,9 +1,11 @@
 import { stdout } from 'node:process';
+import exitApp from './exitApp.js';
 
 const commandListener = async (command) => {
   switch (command) {
     case '.exit':
       console.log('exiting...\n');
+      exitApp();
       break;
     default:
       stdout.write('Operation failed\n');
--- src/modules/userEnter.js
+++ src/modules/userEnter.js
@@ -1,5 +1,7 @@
-import { argv, stdin, stdout } from 'node:process';
+import process from 'node:process';
+import { argv, stdin, stdout, env } from 'node:process';
 import commandListener from './commandListener.js';
+import exitApp from './exitApp.js';
 
 const args = argv.slice(2);
 
@@ -7,10 +9,12 @@ const userEnter = async () => {
   try {
     const username = args.find((arg) => arg.startsWith('--username=')).split('=')[1];
     stdin.write(`Welcome to the File Manager, ${username}!\n\nPlease enter your command below:\n`);
+    env.username = username;
     stdout.on('data', async (data) => {
       const command = data.toString().trim();
       await commandListener(command);
     })
+    process.on('SIGINT', exitApp);
   } catch {
     throw new Error('Entrance error: To run the app, type "npm run start --username=your_username"');
   }
